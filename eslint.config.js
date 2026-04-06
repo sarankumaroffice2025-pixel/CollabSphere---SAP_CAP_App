486cc238ad92fe507edb4cd2ef46d2a2e8472b7c
sarankumaroffice2025-pixel <sarankumaroffice2025@gmail.com>
2026-04-06 15:46:17 +0530

    packagelockjsonUpdate

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["srv/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        Buffer: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      // General
+      "no-console": "warn",
+      "eqeqeq": ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+
+      // TypeScript
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-require-imports": "error",
+    },
+  },
+];
